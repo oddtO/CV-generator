40d44d0f66fda5af33321a5c9716836732a41832
oddtO <121516833+oddtO@users.noreply.github.com>
2024-04-22 21:00:36 +0300

    make text input more generic

--- src/components/text-input/component.jsx
+++ src/components/text-input/component.jsx
@@ -1,9 +1,22 @@
+import PropTypes from "prop-types";
 import styles from "./text-input.module.scss";
-export default function TextInput({ labelText, name }) {
+export default function TextInput({
+  labelText,
+  name,
+  type = "text",
+  regex = "*",
+}) {
   return (
     <label htmlFor={name} className={styles["text-input"]}>
       <span>{labelText}</span>
-      <input type="text" name={name} id={name} />
+      <input type={type} name={name} id={name} pattern={regex} />
     </label>
   );
 }
+
+TextInput.propTypes = {
+  labelText: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  regex: PropTypes.string,
+};
